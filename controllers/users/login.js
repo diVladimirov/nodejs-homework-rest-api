c5187245eb3262a5df5d7854ef45a69aac8a606d
Vadim Vladimirov <kurtonchik@gmail.com>
2022-05-27 19:09:47 +0300

    done signin and login

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -15,7 +15,7 @@ const login = async (req, res) => {
   const payload = {
     id: user._id,
   };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+  const token = jwt.sign(payload, "JKkbgLKJBKJBv23gv", { expiresIn: "1h" });
   res.json({
     status: "success",
     code: 200,
